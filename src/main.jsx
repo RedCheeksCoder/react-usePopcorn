f3664c13f46c6f7d902d973164eab9898c32fe6c
Bryan Odiña <149803435+RedCheeksCoder@users.noreply.github.com>
2023-12-04 14:34:09 +0800

    feature added star-rating

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import './App.css'
-
+/* import App from "./App.jsx";
+import './App.css' */
+import StarRating from "./star.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      color="blue"
+      size={30}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={5}
+    />
+    <Testing />
   </React.StrictMode>
 );
+
+function Testing() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
